ff7eeaa688b7290f8b6c97188aa22dd5e3d626dd
ysutaoteam <104180861+ysutaoteam@users.noreply.github.com>
2022-05-04 17:06:15 +0800

    Update Output the adjacency matrix as an AT graph.py

--- Output the adjacency matrix as an AT graph.py
+++ Output the adjacency matrix as an AT graph.py
@@ -132,16 +132,6 @@ class Graph_Matrix:
 def create_undirected_matrix(my_graph,chuanshu):
     nodes = ['0°-20°', '20°-40°', '40°-60°', '60°-80°', '80°-100°', '100°-120°', '120°-140°', '140°-160°','160°-180°']
 
-    # matrix = [[2, 0, 0, 0, 5, 0, 0, 0, 0],  # a
-    #           [0, 1, 0, 0, 0, 0, 8, 0, 0],  # b
-    #           [0, 1, 3, 1, 0, 6, 0, 0, 0],  # c
-    #           [0, 0, 1, 4, 2, 0, 0, 3, 0],  # d
-    #           [0, 0, 0, 2, 4, 2, 0, 0, 0],  # e
-    #           [0, 0, 0, 0, 2, 5, 2, 0, 0],  # f
-    #           [0, 0, 2, 0, 0, 2, 4, 2, 0],  # g
-    #           [0, 3, 0, 0, 0, 0, 1, 2, 1],  # h
-    #           [0, 0, 5, 0, 0, 0, 0, 0, 1]]  # i
-    #
     matrix = chuanshu
 
     my_graph = Graph_Matrix(nodes, matrix)
@@ -160,10 +150,6 @@ def draw_undircted_graph(my_graph,i,path):
 
     for edge in my_graph.edges:
         G.add_edge(str(edge[0]), str(edge[1]), weight = str(edge[2]))
-
-    # for node in my_graph.vertices:
-    #     G.add_node(str(node))
-    # G.add_weighted_edges_from(my_graph.edges_array)
 
     print("nodes:", G.nodes())  # 输出全部的节点：
     print("edges:", G.edges())  # 输出全部的边：
@@ -202,18 +188,6 @@ def draw_undircted_graph(my_graph,i,path):
             plt.savefig("C:/Users/Administrator/Desktop/AT/yu-1.1/PD" + "/" + path + "/(" + str(shiwei)+")"+str(3)+".jpg")
             plt.clf()#添加上这一行，画完第一个图后，重置一下
 
-    # plt.savefig("F:/HOG-Picture/健康语音每个属性拓扑图/" + str(i) + ".jpg")
-    # plt.savefig("F:/HOG-Picture/(不包含节点名称)健康语音每个属性拓扑图/" + str(i) + ".jpg")
-    # plt.savefig("F:/HOG-Picture/(不包含节点名称)患者语音每个属性拓扑图/" + str(i) + ".jpg")
-    # plt.show()
-
-
-# dataFile = 'F:/HOG-Picture/CPPDD-dataset/SHIYAN/train/0_class/REC001-4-a.jpg.xls' #REC001-4-a.jpg.xls  14(w)-1-a(3).jpg.xls
-# # dataFile_1 = 'F:/HOG-Picture/CPPDD-dataset/SHIYAN/train/1_class/14(w)-1-a(3).jpg.xls' #REC001-4-a.jpg.xls  14(w)-1-a(3).jpg.xls
-# dataFile = 'F:/HOG-Picture/train/0_class/260602_o_3.jpg.xls' #260602_o_4.jpg.xls
-# dataFile_1 = 'F:/HOG-Picture/train/1_class/No2-o(1).jpg.xls' #No2-o(1).jpg.xls  No1-a.jpg.xls
-# data = scio.loadmat(dataFile)
-# data_1 = scio.loadmat(dataFile_1)
 
 for path in os.listdir( 'C:/Users/Administrator/Desktop/xingshibeijing/yu-1.1/0_class'):  #0_class  1_class
     data =scio.loadmat('C:/Users/Administrator/Desktop/xingshibeijing/yu-1.1/0_class' + "./" + path)
@@ -232,46 +206,3 @@ for path in os.listdir( 'C:/Users/Administrator/Desktop/xingshibeijing/yu-1.1/0_
         print(i)
         created_graph,created_weight = create_undirected_matrix(my_graph,shuju[:,9*(i-1):9*i])
         draw_undircted_graph(created_graph,i,path)
-
-
-
-#
-# # 2，有向图
-# G = nx.DiGraph()
-# G.add_node(1)
-# G.add_node(2)
-# G.add_nodes_from([3, 4, 5, 6])
-# G.add_cycle([1, 2, 3, 4])
-# G.add_edge(1, 3)
-# G.add_edges_from([(3, 5), (3, 6), (6, 7)])
-# nx.draw(G,with_labels=True)
-# # plt.savefig("youxiangtu.png")
-# plt.show()
-#
-# # 4，无向图
-# G = nx.Graph()
-# G.add_node(1)
-# G.add_node(2)
-# G.add_nodes_from([3, 4, 5, 6])
-# G.add_cycle([1, 2, 3, 4])
-# G.add_edge(1, 3)
-# G.add_edges_from([(3, 5), (3, 6), (6, 7)])
-# nx.draw(G)
-# # plt.savefig("wuxiangtu.png")
-# plt.show()
-#
-#
-# # 5，颜色节点图
-# G = nx.Graph()
-# G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (4, 5), (4, 6), (5, 6)])
-# pos = nx.spring_layout(G)
-#
-# colors = [1, 2, 3, 4, 5, 6]
-# nx.draw_networkx_nodes(G, pos, node_color=colors)
-# nx.draw_networkx_edges(G, pos)
-#
-# plt.axis('off')
-# # plt.savefig("color_nodes.png")
-# plt.show()
-#
-#
